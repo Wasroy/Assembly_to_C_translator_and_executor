3ef3d347d278ba5454ad7cb6c6de12e2e38aed10
NicolasdePellegars <ndepellegarsmalhortie@gmail.com>
2025-01-18 22:31:34 +0100

    Rayan il mange du couscous

--- Source/simulateur.h
+++ Source/simulateur.h
@@ -1,12 +1,20 @@
 #ifndef SIMULATEUR_H
 #define SIMULATEUR_H
 
-
-
 typedef struct {
-    int opcode; /* Code de l'instruction */
-    int param1; /*Donnée 1*/
-    int param2; /*Donnée 2*/
+    int opcode; 
+    int donnee;
 } instruction; 
+
+int nombreDeLigne(const char* nomfichier);
+void save_code(int tab[], int nligne, instruction* ptr);
+void pop(int x, int *SP);
+void ipop(int *SP);
+void push(int x, tab[], int *SP);
+void ipush(int *SP);
+void push_i (int i, int *SP);
+void jmp(int adr, int *PC);
+void jnz(int adr, int *PC, int *SP);
+void call(int adr);
 
 #endif
