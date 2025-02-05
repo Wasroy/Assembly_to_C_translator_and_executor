7383e4d99a92985359c695267120c9d4ecffb3b2
NicolasdePellegars <ndepellegarsmalhortie@gmail.com>
2025-02-05 16:43:02 +0100

    Update and rename simulateur.c to executeur.c

--- Source/executeur.c
+++ Source/executeur.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "simulateur.h"
+#include "executeur.h"
 
 int nombreDeLigne(const char* nomfichier) {  
 	FILE* file = fopen(nomfichier,"r");
