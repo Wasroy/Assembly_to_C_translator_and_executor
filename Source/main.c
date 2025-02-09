6e46276396f07c03fc8c94413ebced4532006ee3
Nicolas de Pellegars <84878017+leroiarouf0@users.noreply.github.com>
2025-02-09 15:56:57 +0100

    Accouplement des 2 mains

--- Source/main.c
+++ Source/main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "assembleur_to_machine.h"
-#include "assembleur_to_machine.c"
+#include "executeur.h"
+
+
+#define MEM_SIZE  5000
+
+int tab_mem[MEM_SIZE] = {0};
+int PC = 0; 
+int SP = 0;
+int *pPC = &PC;
+int *pSP = &SP;   
+
 
 void menu_trad() {
     printf("\n");
@@ -45,4 +57,39 @@ int main() {
     return traducteur(fichier_assembleur, fichier_hexa);
 
 
+    const char *nomfichier = "hexa.txt";
+
+    int nb_ligne = nombreDeLigne(nomfichier);
+    
+    instruction** tab_ins = (instruction **)malloc(sizeof(instruction*) * (nb_ligne)); 
+    savecode(tab_ins, nomfichier);    
+
+    //Detection d'un possible warning : si il existe un call sans ret ou un ret sans call
+    int nb_call = 0;
+    int nb_ret = 0;
+    for (int i = 0; i < nb_ligne; i++) {
+        if (tab_ins[i]->opcode == 7) {
+            nb_call++;
+        }
+        if (tab_ins[i]->opcode == 8) {
+            nb_ret++;
+        }
+    }
+    if (nb_call != nb_ret) {
+        printf("\033[38;5;214m Warning : Nombre de call et de ret inegaux\033[0m\n");
+    }
+
+    //Début de la boucle qui parcourt le tableau d'instruction jusqu'à ce qu'ont halt() soit appelé
+    while (1) {
+        if (*pPC < 0) {
+            printf("\033[1;31m Erreur : tentative d'accéder à une ligne inexistante (négative) du code\033[0m\n");
+            exit(EXIT_FAILURE);}
+        if (*pPC >= nb_ligne) {
+            printf("\033[1;31m Erreur : tentative d'accéder à une ligne inexistante (superieure au nombre de ligne) du code\033[0m\n");
+            exit(EXIT_FAILURE);
+        }
+        (*pPC)++;
+        executeligne(tab_mem, pSP, pPC, tab_ins, nb_ligne);
+    }
+    return 0; 
 }
